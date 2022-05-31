a8faa22b73413d8e1381188e27e404cccfd668ed
Nicolas2191913 <2191913@csfoy.ca>
2022-05-31 09:39:29 -0400

    Ajout du timer

--- visionneuse/js/scrtipt.js
+++ visionneuse/js/scrtipt.js
@@ -2,7 +2,12 @@
 
 // Variables globales:
 let urlAPI = "https://swapi.dev/api";
+
 let listeFilms = document.getElementById("moviesList");
+let contenu = document.getElementById("content");
+let planet = document.getElementById("planet");
+let planetName = document.getElementById("planetName");
+
 let images = ["alderaan",
             "coruscant",
             "dagobah",
@@ -12,6 +17,14 @@ let images = ["alderaan",
             "yavin iv"];
 
 let films = [];
+let imagesAAfficher = [];
+let imageEnCours = 0;
+
+let timeout;
+let timer;
+let position = 1;
+let delais = [1500, 1000, 500];
+let delaiEnCours = 1;
 
 // Fonctions pour récupérer les données et lister les films:
 function obtenirFilmsSelonPlanette(planet){
@@ -45,6 +58,7 @@ function ajouterFilmAListe(film)
     action.setAttribute("href", "#");
     action.textContent = film;
     listeFilms.appendChild(action);
+    action.addEventListener("click", afficherImages);
 }
 
 (async function obtenirFilms(){
@@ -57,3 +71,24 @@ function ajouterFilmAListe(film)
         .catch(error => console.error(error));
     });
 })();
+
+function afficherImages(e){
+    films.filter(film => {
+        if(film.title == e.target.innerText)
+        {
+            imagesAAfficher = film.planets;
+        }
+    });
+
+    timer = setInterval(changerImage, delais[delaiEnCours]);
+
+    contenu.style.visibility = "visible";
+}
+
+function changerImage()
+{
+    imageEnCours = (imageEnCours + position + imagesAAfficher.length) % imagesAAfficher.length;
+    planet.setAttribute("src", "images/" + imagesAAfficher[imageEnCours] + ".jpg");
+    planet.setAttribute("alt", imagesAAfficher[imageEnCours]);
+    planetName.textContent = imagesAAfficher[imageEnCours];
+}
