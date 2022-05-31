abd87867f6b9894be2e7a7c4e4ff7ed482b1cb11
Nicolas2191913 <2191913@csfoy.ca>
2022-05-30 23:16:45 -0400

    recupération de l'objet films contenant les films et leurs planettes

--- visionneuse/js/scrtipt.js
+++ visionneuse/js/scrtipt.js
@@ -1,2 +1,59 @@
 'use strict'
 
+let urlAPI = "https://swapi.dev/api";
+
+let images = ["alderaan",
+            "coruscant",
+            "dagobah",
+            "endor",
+            "naboo",
+            "tatooine",
+            "yavin iv"];
+
+let listeFilms = document.getElementById("moviesList");
+let films = [];
+
+function obtenirFilmsSelonPlanette(planet){
+    planet.films.forEach(film => {
+        fetch(film)
+        .then(reponse => reponse.json())
+        .then(result => {
+            if(!films.some(film => film.title == result.title))
+            {
+                films.push({'title': result.title, 'planets': [planet.name]});
+            }
+            else
+            {
+                films.filter(film => {
+                    if(film.title == result.title)
+                    {
+                        film.planets.push(planet.name);
+                    }
+                });
+            }
+        })
+        .catch(error => console.error(error));
+    });
+}
+
+(async function obtenirFilms(){
+    let url = urlAPI + "/planets/?search=";
+    images.forEach(planette => {
+        fetch(url + planette)
+        .then(reponse => reponse.json())
+        .then(result => result.results[0])
+        .then(planet => obtenirFilmsSelonPlanette(planet))
+        .catch(error => console.error(error));
+    });
+})();
+
+// function ajouterFilmAListe(film)
+// {
+//     let action = document.createElement("a");
+//     action.classList.add("dropdown-item");
+//     action.setAttribute("href", "#");
+//     action.textContent = film.title;
+//     listeFilms.appendChild(action);
+// }
+
+
