5bdd016e42b35c2ab40538552d3445ef890f5a60
Nicolas2191913 <2191913@csfoy.ca>
2022-06-02 00:30:51 -0400

    Loader fonctionnel, optimisation du nombre de fetch, nettoyage

--- visionneuse/js/scrtipt.js
+++ visionneuse/js/scrtipt.js
@@ -19,6 +19,10 @@ let fichiersImages = ["alderaan",
                 "yavin iv"];
 
 let films = [];
+
+let nombrePlanettesTraitees = 0;
+var filmsTraites;
+
 let imagesAAfficher = [];
 let imageEnCours = 0;
 
@@ -27,39 +31,73 @@ let position = 1;
 let delais = [1500, 1000, 500];
 let delaiEnCours = 1;
 
+let erreurChargement = false;
+
 // Évenements:
 document.getElementById("precedent").addEventListener("click", passerImageSuivante);
 document.getElementById("suivant").addEventListener("click", passerImagePrecedente);
 boutonRotation.addEventListener("click", activerDesactiverDiaporama);
 boutonsVitesseRotation.forEach(boutonRadio => boutonRadio.addEventListener("click", changerVitesse));
 
-// Fonctions pour récupérer les données et lister les films:
-function obtenirFilmsSelonPlanette(planet){
-    planet.films.forEach(film => {
-        fetch(film)
+// Initialisation de la page:
+obtenirPlanettes();
+
+// Fonctions pour récupérer les données des planettes et lister les films:
+function obtenirPlanettes(){
+    let url = urlAPI + "/planets/?search=";
+    fichiersImages.forEach(async planette => {
+        await fetch(url + planette)
         .then(reponse => reponse.json())
-        .then(result => {
-            if(!films.some(film => film.title == result.title))
-            {
-                films.push({'title': result.title, 'planets': [planet.name.toLowerCase()]});
-                ajouterFilmAListe(result.title);
-            }
-            else
-            {
-                films.filter(film => {
-                    if(film.title == result.title)
-                    {
-                        film.planets.push(planet.name.toLowerCase());
-                    }
-                });
-            }
+        .then(result => result.results[0])
+        .then(planet => {
+            obtenirFilmsSelonPlanette(planet);
+            nombrePlanettesTraitees++;
         })
-        .catch(error => console.error(error));
+        .catch(error => {
+            console.log('error: ', error);
+            erreurChargement = true;
+            afficherErreur();
+        });
+    });
+}
+
+function obtenirFilmsSelonPlanette(planet){
+    planet.films.forEach(async urlFilm => {
+        filmsTraites = films.filter(film => film.url == urlFilm);
+        if(filmsTraites.length > 0)
+        {
+            filmsTraites[0]['planets'].push(planet.name.toLowerCase());
+        }
+        else
+        {
+            films.push({'title': '', 'url': urlFilm, 'planets': [planet.name.toLowerCase()]});
+            await fetch(urlFilm)
+            .then(reponse => reponse.json())
+            .then(result => {
+                 //L'appel asynchrone ne semble pas permettre la persistance des variables globales ou locales assignées avant la réponse obtenue
+                 //si d'autres promesses ont été traitées entre temps, pour l'exemple avec filmsTraitres déclarée globalement:
+                 //filmsTraites[0]['title'] = result.title; => ne fonctionne pas.
+                films.filter(film => film.url == urlFilm)[0]['title'] = result.title;
+                ajouterFilmAListe(result.title);
+            })
+            .catch(error => {
+                console.log('error: ', error);
+                erreurChargement = true;
+                afficherErreur();
+            });
+        }
+        if(planet.films.indexOf(urlFilm) == planet.films.length - 1)
+        {
+            verifierChargementTermine();
+        }
     });
 }
 
 function ajouterFilmAListe(film)
 {
+    // Elements de liste avec un style "custom" de BootStrap, une "<div>" remplace le "<select>"
+    // et les "<a>" remplacent les <option>. L'assignation de l'attribut "href" semble obligatoire
+    // pour un bon fonctionnement.
     let action = document.createElement("a");
     action.classList.add("dropdown-item");
     action.setAttribute("href", "#");
@@ -68,17 +106,25 @@ function ajouterFilmAListe(film)
     action.addEventListener("click", afficherImages);
 }
 
-(async function obtenirFilms(){
-    let url = urlAPI + "/planets/?search=";
-    fichiersImages.forEach(planette => {
-        fetch(url + planette)
-        .then(reponse => reponse.json())
-        .then(result => result.results[0])
-        .then(planet => obtenirFilmsSelonPlanette(planet))
-        .catch(error => console.error(error));
-    });
-})();
+//Fonctions pour afficher le contenu ou erreur s'il y a, après le chargement des données:
+function verifierChargementTermine()
+{
+    if(nombrePlanettesTraitees == fichiersImages.length && erreurChargement == false)
+    {
+        afficherContenu();
+    }
+}
 
+function afficherContenu()
+{
+    document.getElementById("loader").style.visibility = "hidden";
+    document.getElementById("movies").style.visibility = "visible";
+}
+
+function afficherErreur()
+{
+    document.getElementById("loaderText").textContent = "Erreur de chargement";
+}
 
 // Fonctions pour afficher les images:
 function afficherImages(e){
@@ -109,14 +155,20 @@ function passerImageSuivante()
 {
     position = 1;
     changerImage();
-    lancerTimer();
+    if(timer !== null)
+    {
+        lancerTimer();
+    }
 }
 
 function passerImagePrecedente()
 {
     position = -1;
     changerImage();
-    lancerTimer();
+    if(timer !== null)
+    {
+        lancerTimer();
+    }
 }
 
 // Fonctions pour le diaporama:
@@ -151,6 +203,7 @@ function desactiverDiaporama()
     timer = null;
 }
 
+// Fonctions pour le timer:
 function changerVitesse(e)
 {
     delaiEnCours = Array.from(boutonsVitesseRotation).indexOf(e.target);
